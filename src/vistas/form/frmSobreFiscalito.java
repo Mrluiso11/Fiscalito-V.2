3b756739758964c5be6fa080f0844091c7ddc811
MichaelC27 <dbpanama2796@gmail.com>
2023-11-30 14:32:12 -0500

    Se le dio funcionalidad el boton manual de usuario

--- src/vistas/form/frmSobreFiscalito.java
+++ src/vistas/form/frmSobreFiscalito.java
@@ -4,7 +4,6 @@
  */
 package vistas.form;
 
-
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 import Style.Forms;
@@ -13,6 +12,7 @@ import conexion.Conexion;
 import java.sql.Connection;
 import controladores.*;
 import documentgeneration.FacturaPDF;
+import java.awt.Desktop;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -26,6 +26,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DecimalFormat;
 import java.text.ParseException;
+
 /**
  *
  * @author admin
@@ -37,9 +38,10 @@ public class frmSobreFiscalito extends javax.swing.JPanel {
      */
     public frmSobreFiscalito() {
         initComponents();
-            txtaSobre.setEditable(false);
-            txtaSobre.setOpaque(false); // Quita el fondo
-            txtaSobre.setBorder(null); 
+
+        txtaSobre.setEditable(false);
+        txtaSobre.setOpaque(false); // Quita el fondo
+        txtaSobre.setBorder(null);
     }
 
     /**
@@ -54,10 +56,12 @@ public class frmSobreFiscalito extends javax.swing.JPanel {
         bg = new javax.swing.JPanel();
         lblLogo = new javax.swing.JLabel();
         btnConoce = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
         txtaSobre = new javax.swing.JTextArea();
 
-        bg.setOpaque(false);
+        setBackground(new java.awt.Color(245, 245, 245));
+        setOpaque(false);
+
+        bg.setBackground(new java.awt.Color(245, 245, 245));
         bg.setVerifyInputWhenFocusTarget(false);
         bg.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
@@ -66,24 +70,25 @@ public class frmSobreFiscalito extends javax.swing.JPanel {
         lblLogo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/logo_fiscalito_final_mini.png"))); // NOI18N
         bg.add(lblLogo, new org.netbeans.lib.awtextra.AbsoluteConstraints(430, 60, 200, 70));
 
-        btnConoce.setBackground(new java.awt.Color(102, 153, 255));
+        btnConoce.setBackground(new java.awt.Color(9, 158, 249));
         btnConoce.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         btnConoce.setForeground(new java.awt.Color(255, 255, 255));
         btnConoce.setText("Conoce Sobre");
+        btnConoce.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnConoceActionPerformed(evt);
+            }
+        });
         bg.add(btnConoce, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 420, 170, 50));
 
-        jScrollPane1.setBackground(new java.awt.Color(242, 242, 242));
-        jScrollPane1.setBorder(null);
-
+        txtaSobre.setBackground(new java.awt.Color(245, 245, 245));
         txtaSobre.setColumns(20);
         txtaSobre.setFont(new java.awt.Font("Century Gothic", 1, 18)); // NOI18N
         txtaSobre.setLineWrap(true);
         txtaSobre.setRows(6);
         txtaSobre.setText("Fiscalito inicia en el 2013 con la misión facilitar el manejo de facturación fiscal por medio de su software. Nuestro proyecto en el 2023 consta del desarrollo y actualización de este programa de facturación fiscal con la Version 2.0, se diseñaron nuevas características y mejoras en su interfaz para que sea más amigable con el usuario al momento de utilizar programa de facturación fiscal.  Esto podría incluir la automatización de tareas repetitivas, la generación de informes fiscales específicos o la mejora de la usabilidad general del software. Adicional, la migración de datos de una base de datos antigua como FoxPro a SQL Lite para su administración correspondiente.");
         txtaSobre.setWrapStyleWord(true);
-        jScrollPane1.setViewportView(txtaSobre);
-
-        bg.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 180, 880, 190));
+        bg.add(txtaSobre, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 140, 800, 250));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -97,11 +102,33 @@ public class frmSobreFiscalito extends javax.swing.JPanel {
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnConoceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConoceActionPerformed
+        try {
+            // Especifica la ubicación del archivo PDF dentro del paquete
+            String rutaPDF = "/ManualUsuario/Manual_de_Usuario_Fiscalito_V.2.pdf";
+
+            // Obtiene la URL del recurso
+            java.net.URL pdfURL = getClass().getResource(rutaPDF);
+
+            // Verifica si la URL es válida
+            if (pdfURL != null) {
+                // Crea un objeto File con la URL
+                java.io.File pdfFile = new java.io.File(pdfURL.toURI());
+
+                // Verifica si el escritorio es compatible para abrir el archivo
+                if (Desktop.isDesktopSupported()) {
+                    Desktop.getDesktop().open(pdfFile);
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }//GEN-LAST:event_btnConoceActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel bg;
     private javax.swing.JButton btnConoce;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblLogo;
     private javax.swing.JTextArea txtaSobre;
     // End of variables declaration//GEN-END:variables
